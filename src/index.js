d0796af34ecc4d6a43ccd53d3e9916fa7e0c2779
AbdumurodovaZulfizar <abdumurodovazulfizar77@gmail.com>
2021-08-31 15:57:45 +0500

    Set up webpack with assets menagment

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import _ from 'lodash';
+import './style.css';
+import Thorn from './thorn.png'
+
 function component() {
   const element = document.createElement('div');
 
   // Lodash, currently included via a script, is required for this line to work
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
+  const myIcon = new Image();
+  myIcon.src = Thorn;
+
+  element.appendChild(myIcon);
 
   return element;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -6,4 +6,15 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  module: {
+    rules: [{
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
 };
