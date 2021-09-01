a3faaebaa7df5e0a1126aef58de0972a4fe4a49e
AbdumurodovaZulfizar <abdumurodovazulfizar77@gmail.com>
2021-09-01 20:31:06 +0500

    Display game on tablet

--- src/index.js
+++ src/index.js
@@ -1,4 +1,30 @@
+// eslint-disable-next-line import/no-unresolved
+import Phaser from 'phaser';
 import './style.css';
-import axios from 'axios';
+import gameState from './helpers/gameState';
+import Start from './scenes/start';
+import InputName from './scenes/inputName';
+import Play from './scenes/play';
+import GameOver from './scenes/gameOver';
+import Score from './scenes/score';
 
-FN1ZIQ0tRBnXA1C9RtBj --- Game ID
+const config = {
+  type: Phaser.AUTO,
+  parent: 'phaser-example',
+  dom: {
+    createContainer: true,
+  },
+  width: gameState.canvas.width,
+  height: gameState.canvas.height,
+  backgroundColor: 'b9eaff',
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { y: 400 },
+      enableBody: true,
+    },
+  },
+  scene: [Start, InputName, Play, GameOver, Score],
+};
+
+gameState.game = new Phaser.Game(config);
